bb6126c67569b8030216f0a1ed0ea3e5de698b19
Branaaa <120464626+Branaaa@users.noreply.github.com>
2022-12-19 23:25:59 +0100

    asd

---
+++ HyperCasual/Assets/Scripts/UIButtonAnimation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class UIButtonAnimation : MonoBehaviour
+{
+	public bool wasClicked;
+    public void AnimateButton()
+{
+	if(!wasClicked){
+
+	transform.DOPunchScale(new Vector3(-0.2f, -0.2f, -0.2f), 0.3f, 1, 0.5f);
+	StartCoroutine(DelayTimer());
+	}
+}
+	public IEnumerator DelayTimer()
+	{
+		wasClicked=true;
+		yield return new WaitForSeconds(0.3f);
+		wasClicked=false;
+		
+}	
+
+
+}
